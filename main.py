7e7fbed898ab1fa0c86720120d54da2814b448eb
ananta.joy <anantajoy007@gmail.com>
2023-08-07 12:53:10 +0600

    OpenAI API testing

---
+++ main.py
@@ -0,0 +1,21 @@
+# first OpenAPI request
+import os
+from dotenv import load_dotenv
+import openai
+
+
+# os.getenv('api_key')
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+
+# api key
+openai.api_key = api_key
+
+# create a request to complete the answer
+response = openai.Completion.create(
+    model='text-davinci-003',
+    prompt='Who id the Prime Minister of Bangladesh?'
+)
+
+print(response)
